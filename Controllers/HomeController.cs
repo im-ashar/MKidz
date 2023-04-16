e7f710f2f8d51dbb730fa31d89926c75650f74c0
im-ashar <muhammadashar.401@gmail.com>
2023-04-16 15:32:08 +0500

    fixing sort and change button color

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -38,16 +38,44 @@ namespace MKidz.Controllers
             ViewBag.folderName = folderName;
             audioFiles.Sort((a, b) =>
             {
-                int aNumber, bNumber;
-                if (int.TryParse(Regex.Match(a.Name, @"\d+").Value, out aNumber) && int.TryParse(Regex.Match(b.Name, @"\d+").Value, out bNumber))
+                string[] aNameParts = a.Name.Split('.');
+                string[] bNameParts = b.Name.Split('.');
+                string aNumericPart, bNumericPart;
+                int aNumericValue, bNumericValue;
+
+                // Get the first numeric part of a's name
+                aNumericPart = Regex.Match(aNameParts[0], @"\d+").Value;
+                // If there is no numeric part, set aNumericValue to int.MaxValue so that it will be sorted to the end
+                aNumericValue = aNumericPart == "" ? int.MaxValue : int.Parse(aNumericPart);
+
+                // Get the first numeric part of b's name
+                bNumericPart = Regex.Match(bNameParts[0], @"\d+").Value;
+                // If there is no numeric part, set bNumericValue to int.MaxValue so that it will be sorted to the end
+                bNumericValue = bNumericPart == "" ? int.MaxValue : int.Parse(bNumericPart);
+
+                // Compare the numeric parts of a and b
+                int comparisonResult = aNumericValue.CompareTo(bNumericValue);
+                if (comparisonResult != 0)
                 {
-                    return aNumber.CompareTo(bNumber);
+                    // If the numeric parts are different, return the comparison result
+                    return comparisonResult;
                 }
-                else
+
+                // If the numeric parts are the same, compare the rest of the name
+                string aNonNumericPart = aNameParts[0].Substring(aNumericPart.Length);
+                string bNonNumericPart = bNameParts[0].Substring(bNumericPart.Length);
+                comparisonResult = string.Compare(aNonNumericPart, bNonNumericPart, StringComparison.OrdinalIgnoreCase);
+                if (comparisonResult != 0)
                 {
-                    return a.Name.CompareTo(b.Name);
+                    // If the non-numeric parts are different, return the comparison result
+                    return comparisonResult;
                 }
+
+                // If the non-numeric parts are also the same, compare the file extensions
+                return string.Compare(aNameParts[1], bNameParts[1], StringComparison.OrdinalIgnoreCase);
             });
+
+
 
 
 
